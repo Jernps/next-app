15a50016fb32a9c48fec81043970664fc7e44862
Jernps <44694512+Jernps@users.noreply.github.com>
2023-10-18 16:54:21 +0700

    update auth module

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,15 +1,24 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { NextAuthOptions } from "next-auth";
 
-export const authOptions = {
+const prisma = new PrismaClient();
+
+export const authOptions : NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers:[
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
-    ]
+    ],
+    session:{
+        strategy: 'jwt'
+    }
 }
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions );
 
 export {handler as GET, handler as POST}
